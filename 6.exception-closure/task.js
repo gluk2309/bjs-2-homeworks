3dd276ae6b6646ded3c4634b2b7b5c8b1358697a
gluk2309 <gluk2309@mail.ru>
2023-12-27 12:37:50 +0300

    Two tasks solved

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿/*################# Task 1 #################*/
+
+function parseCount(parseTheValue) {
+  const value = Number.parseFloat(parseTheValue);
+  if (isNaN(value)) {
+    throw new Error("Невалидное значение");
+  } else {
+    return value;
+  }
+}
+function validateCount(parseTheValue) {
+  try {
+    return parseCount(parseTheValue);
+  } catch (error) {
+    return error;
+  }
+}
+
+/*################## Task 2 ##################*/
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a > b + c 
+      || b > a + c 
+      || c > a + b) {
+
+      throw new Error("Треугольник с такими сторонами не существует");
+    } else {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+  }
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    let p = this.perimeter / 2;
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
+
